ceddef7ff5b9c1a26828d22957971cabbbb6f00a
djava6 <67018259+djava6@users.noreply.github.com>
2020-06-16 14:43:41 -0300

    Commit inicial

---
+++ dimdimdim.bat
@@ -0,0 +1,3 @@
+@echo off
+set CLASSPATH=lib/ant-antlr-1.6.5.jar;lib/antlr-2.7.6rc1.jar;lib/asm.jar;lib/asm-attrs.jar;lib/cglib-2.1.3.jar;lib/commons-collections-2.1.1.jar;lib/commons-logging-1.0.4.jar;lib/dom4j-1.6.1.jar;lib/jta.jar;lib/log4j-1.2.11.jar;lib/hibernate3.jar;lib/hsqldb.jar;lib/jcalendarbutton.jar;dist/dimdimdim.jar
+start javaw br.com.useinet.main.DimDimMain
